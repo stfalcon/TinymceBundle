c983ec823f31fca6821107c81b0f2abab4cd0ffa
Andrey Shkodyak <fluxuator@gmail.com>
2012-09-28 14:36:55 +0300

    Merge pull request #59 from axzx/patch-1

--- Twig/Extension/StfalconTinymceExtension.php
+++ Twig/Extension/StfalconTinymceExtension.php
@@ -90,8 +90,8 @@ class StfalconTinymceExtension extends \Twig_Extension
 
         // If the language is not set in the config...
         if (!isset($config['language']) || empty($config['language'])) {
-            // get it from the session
-            $config['language'] = $this->getService('session')->get('locale');
+            // get it from the request
+            $config['language'] = $this->getService('request')->getLocale();
         }
 
         // Check the language code and trim it to 2 symbols (en_US to en, ru_RU to ru, ...)
