7efdb9b3477463991d8689a98d48021238a23df2
Steffen Roßkamp <steffen.rosskamp@gimmickmedia.de>
2014-06-12 19:20:37 +0200

    Move TinyMCE languages to own directory Update language file handling to check for existing default territory expansion Remove embedded TinyMCE Add symlink and mirror commands for external TinyMCE Add symlink and mirror script handler for composer

---
+++ Composer/ScriptHandler.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Stfalcon\Bundle\TinymceBundle\Composer;
+
+use Composer\Script\Event;
+use Mopa\Bridge\Composer\Util\ComposerPathFinder;
+use Stfalcon\Bundle\TinymceBundle\Command\TinymceSymlinkCommand;
+
+/**
+ * Script for Composer, create symlink to tinymce lib into the TinymceBundle.
+ */
+class ScriptHandler
+{
+    public static function postInstallSymlinkTinymce(Event $event)
+    {
+        $IO = $event->getIO();
+        $composer = $event->getComposer();
+        $cmanager = new ComposerPathFinder($composer);
+        $options = array(
+            'targetSuffix' => self::getTargetSuffix(),
+            'sourcePrefix' => '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR
+        );
+        list($symlinkTarget, $symlinkName) = $cmanager->getSymlinkFromComposer(
+            TinymceSymlinkCommand::$stfalconTinymceBundleName,
+            TinymceSymlinkCommand::$tinymceName,
+            $options
+        );
+
+        $IO->write("Checking Symlink", FALSE);
+        if (false === TinymceSymlinkCommand::checkSymlink($symlinkTarget, $symlinkName, true)) {
+            $IO->write("Creating Symlink: " . $symlinkName, FALSE);
+            TinymceSymlinkCommand::createSymlink($symlinkTarget, $symlinkName);
+        }
+        $IO->write(" ... <info>OK</info>");
+    }
+
+    public static function postInstallMirrorTinymce(Event $event)
+    {
+        $IO = $event->getIO();
+        $composer = $event->getComposer();
+        $cmanager = new ComposerPathFinder($composer);
+        $options = array(
+            'targetSuffix' =>  self::getTargetSuffix(),
+            'sourcePrefix' => '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR
+        );
+        list($symlinkTarget, $symlinkName) = $cmanager->getSymlinkFromComposer(
+            TinymceSymlinkCommand::$stfalconTinymceBundleName,
+            TinymceSymlinkCommand::$tinymceName,
+            $options
+        );
+
+        $IO->write("Checking Mirror", FALSE);
+        if (false === TinymceSymlinkCommand::checkSymlink($symlinkTarget, $symlinkName)) {
+            $IO->write("Creating Mirror: " . $symlinkName, FALSE);
+            TinymceSymlinkCommand::createMirror($symlinkTarget, $symlinkName);
+        }
+        $IO->write(" ... <info>OK</info>");
+    }
+
+    protected static function getTargetSuffix($end = "")
+    {
+        return DIRECTORY_SEPARATOR . "Resources" . DIRECTORY_SEPARATOR . "public" . DIRECTORY_SEPARATOR . "vendor". DIRECTORY_SEPARATOR . "tinymce" . $end;
+    }
+}
--- Twig/Extension/StfalconTinymceExtension.php
+++ Twig/Extension/StfalconTinymceExtension.php
@@ -107,7 +107,7 @@ class StfalconTinymceExtension extends \Twig_Extension
             $config['language'] = $this->getService('request')->getLocale();
         }
 
-        $langDirectory = __DIR__ . '/../../Resources/public/vendor/tinymce/langs/';
+        $langDirectory = __DIR__ . '/../../Resources/public/vendor/tinymce-langs/';
 
         // A language code coming from the locale may not match an existing language file
         if (!file_exists($langDirectory . $config['language'] . '.js')) {
@@ -123,9 +123,12 @@ class StfalconTinymceExtension extends \Twig_Extension
             } else {
                 // Try expanding the code
                 $longCode = $config['language'] . '_' . strtoupper($config['language']);
+                $expandedCode = $this->expandLocale($config['language']);
 
                 if (file_exists($langDirectory . $longCode . '.js')) {
                     $config['language'] = $longCode;
+                } elseif (file_exists($langDirectory . $expandedCode . '.js')) {
+                    $config['language'] = $expandedCode;
                 } else {
                     unset($config['language']);
                 }
@@ -133,10 +136,15 @@ class StfalconTinymceExtension extends \Twig_Extension
         }
 
         if (isset($config['language']) && $config['language']) {
+            $languageUrl = $assets->getUrl(
+                $this->baseUrl . 'bundles/stfalcontinymce/vendor/tinymce-langs/' . $config['language'] . '.js'
+            );
             // TinyMCE does not allow to set different languages to each instance
             foreach ($config['theme'] as $themeName => $themeOptions) {
                 $config['theme'][$themeName]['language'] = $config['language'];
+                $config['theme'][$themeName]['language_url'] = $languageUrl;
             }
+            $config['language_url'] = $languageUrl;
         }
 
         return $this->getService('templating')->render('StfalconTinymceBundle:Script:init.html.twig', array(
@@ -177,6 +185,34 @@ class StfalconTinymceExtension extends \Twig_Extension
         }
 
         return $inputUrl;
+    }
+
+    /**
+     * Expands a short locale to a long one
+     *
+     * @param string $locale
+     * @return string
+     */
+    protected function expandLocale($locale)
+    {
+        $conversion = array(
+            'bn' => 'bn_BD',
+            'en' => 'en_GB',
+            'he' => 'he_IL',
+            'ka' => 'ka_GE',
+            'km' => 'km_KH',
+            'ko' => 'ko_KR',
+            'nb' => 'nb_NO',
+            'si' => 'si_LK',
+            'sl' => 'sl_SI',
+            'sv' => 'sv_SE',
+            'zh' => 'zh_CN',
+        );
+        if (array_key_exists($locale, $conversion)) {
+            return $conversion[$locale];
+        }
+
+        return $locale;
     }
 }
 
