8062eb74b913fd2e4571408aaf10c4cc5239b750
Christoph Kretzschmar <ck@robhost.de>
2014-09-12 10:39:02 +0200

    Support multiple assets in custom_css parsing

--- Twig/Extension/StfalconTinymceExtension.php
+++ Twig/Extension/StfalconTinymceExtension.php
@@ -140,7 +140,16 @@ class StfalconTinymceExtension extends \Twig_Extension
             foreach ($config['theme'] as $themeName => $themeOptions) {
                 if(isset($themeOptions['content_css']))
                 {
-                    $config['theme'][$themeName]['content_css'] = $this->getAssetsUrl($themeOptions['content_css']);
+                    // As there may be multiple CSS Files specified we need to parse each of them individually
+                    $cssFiles = explode(',', $themeOptions['content_css']);
+
+                    foreach($cssFiles as $idx => $file)
+                    {
+                        $cssFiles[$idx] = $this->getAssetsUrl(trim($file)); // we trim to be sure we get the file without spaces.
+                    }
+
+                    // After parsing we add them together again.
+                    $config['theme'][$themeName]['content_css'] = implode(',', $cssFiles);
                 }
             }
         }
