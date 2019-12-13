0c9e076024d2f3103dfed1e82806155983ddfc92
Pavlo Popov <pavlo.popov@stfalcon.com>
2019-12-13 13:57:41 +0200

    Allow multiple selectors (array of selectors) #237

--- Resources/public/js/init.standard.js
+++ Resources/public/js/init.standard.js
@@ -8,33 +8,29 @@ function initTinyMCE(options) {
     if (typeof options == 'undefined') options = stfalcon_tinymce_config;
     // Load when DOM is ready
     domready(function() {
-        var i, t = tinymce.editors, textareas = [];
+        let i, t = tinymce.editors, textareas = [];
         for (i in t) {
             if (t.hasOwnProperty(i)) t[i].remove();
         }
-        switch (options.selector.substring(0, 1)) {
-            case "#":
-                var _t = document.getElementById(options.selector.substring(1));
-                if (_t) textareas.push(_t);
-                break;
-            case ".":
-                textareas = document.getElementsByClassName(options.selector.substring(1));
-                break;
-            default :
-                textareas = document.getElementsByTagName('textarea');
+        if(Array.isArray(options.selector)) {
+            options.selector.forEach(function(selector) {
+                textareas = processSelector(selector, textareas);
+            });
+        } else {
+            textareas = processSelector(options.selector, textareas);
         }
         if (!textareas.length) {
             return false;
         }
 
-        var externalPlugins = [];
+        let externalPlugins = [];
         // Load external plugins
         if (typeof options.external_plugins == 'object') {
-            for (var pluginId in options.external_plugins) {
+            for (let pluginId in options.external_plugins) {
                 if (!options.external_plugins.hasOwnProperty(pluginId)) {
                     continue;
                 }
-                var opts = options.external_plugins[pluginId],
+                let opts = options.external_plugins[pluginId],
                     url = opts.url || null;
                 if (url) {
                     externalPlugins.push({
@@ -48,28 +44,28 @@ function initTinyMCE(options) {
 
         for (i = 0; i < textareas.length; i++) {
             // Get editor's theme from the textarea data
-            var theme = textareas[i].getAttribute("data-theme") || 'simple';
+            let theme = textareas[i].getAttribute("data-theme") || 'simple';
             // Get selected theme options
-            var settings = (typeof options.theme[theme] != 'undefined')
+            let settings = (typeof options.theme[theme] != 'undefined')
                 ? options.theme[theme]
                 : options.theme['simple'];
 
             settings.external_plugins = settings.external_plugins || {};
-            for (var p = 0; p < externalPlugins.length; p++) {
+            for (let p = 0; p < externalPlugins.length; p++) {
                 settings.external_plugins[externalPlugins[p]['id']] = externalPlugins[p]['url'];
             }
             // workaround for an incompatibility with html5-validation
             if (textareas[i].getAttribute("required") !== '') {
                 textareas[i].removeAttribute("required")
             }
-            var textAreaId = textareas[i].getAttribute('id');
+            let textAreaId = textareas[i].getAttribute('id');
             if (textAreaId === '' || textAreaId === null) {
                 textareas[i].setAttribute("id", "tinymce_" + Math.random().toString(36).substr(2));
             }
             // Add custom buttons to current editor
             if (typeof options.tinymce_buttons == 'object') {
                 settings.setup = function(editor) {
-                    for (var buttonId in options.tinymce_buttons) {
+                    for (let buttonId in options.tinymce_buttons) {
                         if (!options.tinymce_buttons.hasOwnProperty(buttonId)) continue;
 
                         // Some tricky function to isolate variables values
@@ -89,7 +85,7 @@ function initTinyMCE(options) {
                     //Init Event
                     if (options.use_callback_tinymce_init) {
                         editor.on('init', function() {
-                            var callback = window['callback_tinymce_init'];
+                            let callback = window['callback_tinymce_init'];
                             if (typeof callback == 'function') {
                                 callback(editor);
                             } else {
@@ -108,16 +104,35 @@ function initTinyMCE(options) {
 }
 
 /**
+ * @param selector
+ * @param textareas
+ */
+function processSelector(selector, textareas) {
+    switch (selector.substring(0, 1)) {
+        case "#":
+            let _t = document.getElementById(selector.substring(1));
+            if (_t) textareas.push(_t);
+            break;
+        case ".":
+            textareas = textareas.concat(document.getElementsByClassName(selector.substring(1)));
+            break;
+        default:
+            textareas = textareas.concat(document.getElementsByTagName('textarea'));
+    }
+    return textareas;
+}
+
+/**
  * Get elements by class name
  *
  * @param classname
  * @param node
  */
 function getElementsByClassName(classname, node) {
-    var elements = document.getElementsByTagName(node),
+    let elements = document.getElementsByTagName(node),
         array = [],
         re = new RegExp('\\b' + classname + '\\b');
-    for (var i = 0, j = elements.length; i < j; i++) {
+    for (let i = 0, j = elements.length; i < j; i++) {
         if (re.test(elements[i].className)) array.push(elements[i]);
     }
     return array;
@@ -132,7 +147,7 @@ function clone(o) {
     if (!o || "object" !== typeof o) {
         return o;
     }
-    var c = "function" === typeof o.pop ? [] : {}, p, v;
+    let c = "function" === typeof o.pop ? [] : {}, p, v;
     for (p in o) {
         if (o.hasOwnProperty(p)) {
             v = o[p];
